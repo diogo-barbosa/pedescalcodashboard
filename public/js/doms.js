3874f4a24029fd020c6afade852427c557a6d757
Diogo Barbosa <developer.dbarbosa@gmail.com>
2018-08-03 16:06:28 +0100

    Added tickets

--- public/js/doms.js
+++ public/js/doms.js
@@ -43,14 +43,56 @@ var homeSection = `<div class="heading">
 <table class="table">
 <thead class="thead-dark">
     <th>Cor Identificadora</th>
-    <th>Número de Pessoas</th>
-    <th>Preço Total</th>
-    <th>Data</th>
+    <th>Bilhete</th>
+    <th>Preço</th>
+    <th>Data e Hora</th>
 </thead>
 <tbody class="table-body">
     
 </tbody>
 </table>`;
+
+var register = `<div class="heading">
+<h1>Bilheteira</h1>
+<p>Aqui é feito o registo das entradas no parque.</p>
+</div>
+
+<form class="ticket-form">
+<div class="form-group">
+    <label for="color">Cor Identificadora</label>
+    <select id="color" class="form-control color">
+        <option selected style="background: blue; color: blue;" value="blue">Azul</option>
+        <option style="background: red; color: red;" value="red">Vermelho</option>
+        <option style="background: black; color: black;" value="black">Preto</option>
+    </select>
+</div>
+<div class="form-group">
+        <label for="ticket">Bilhete</label>
+        <select id="ticket" class="form-control ticket">
+            
+        </select>
+</div>
+<div class="form-row">
+    <div class="form-group col-md-9">
+        <label for="price">Preço</label>
+        <input type="number" readonly value="5" required class="form-control price">
+    </div>
+    <div class="form-group col-md-3">
+        <label for="manual">Definir Manualmente</label>
+        <input type="checkbox" class="form-control manually">
+    </div>
+</div>
+
+<div class="form-group">
+    <label for="date">Data de Registo</label>
+    <input required type="date" class="form-control date-input">
+</div>
+<div class="form-group">
+    <label for="time">Hora de Registo</label>
+    <input required type="time" class="form-control time-input">
+</div>
+<button type="submit" class="btn btn-primary register-button">Registar</button>
+</form>`;
 
 var tableRows = `<tr>
 <td style="background: red"></td>
@@ -58,3 +100,13 @@ var tableRows = `<tr>
 <td>25€</td>
 <td>02/08/2018</td>
 </tr>`;
+
+var blueTickets = `<option selected value="0">Individual (1 pessoa)</option>
+<option value="1">Duplo (2 pessoas)</option>
+<option value="2">Grupo (3 a 4 pessoas)</option>`;
+
+var redTickets = `<option selected value="3">Pack Duplo (2 pessoas)</option>
+<option value="4">Pack Familiar (3 a 4 pessoas)</option>
+<option value="5">Pack Grupo Grande (Máx. 8 pessoas)</option>`;
+
+var blackTickets = "<option selected value='6'>Bilhete Laranja-Limão</option>"
--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,10 @@ $(document).ready(function(){
     $('.content-area').html(getContent(currentPage));
     updateTable();
 
+    //----DEBUG----
+    
+    //-------------
+
     //----GLOBAL----
     $('.nav-button').click(function(){
         $('.active').removeClass('active');
@@ -13,6 +17,11 @@ $(document).ready(function(){
         $('.content-area').html(getContent(currentPage));
         if(currentPage == "home") {
             updateTable();
+        } else if(currentPage == "register"){
+            currentPage = "register";
+            $('.date-input').val(getDate());
+            $('.time-input').val(getTime());
+            updateTickets();
         }
     });
     //--------------
@@ -29,6 +38,16 @@ $(document).ready(function(){
         }
     });
     //------------
+
+    //----REGISTER----
+    $('.manually').change(function(){
+        $('.price').attr("readonly", !this.checked);
+    });
+
+    $('.color').change(function(){updateTickets();});
+    $('.ticket').change(function(){updatePrice();});
+
+    //----------------
 });
 
 var getContent = function(id){
@@ -36,6 +55,8 @@ var getContent = function(id){
         case "home":
             return homeSection;
             break;
+        case "register":
+            return register;
         default:
             return "Not implemented yet"; 
     }
@@ -45,5 +66,79 @@ var updateTable = function(){
     //getTableContent();
     if(currentPage == "home"){
         $('.table-body').html(tableRows);
+    }
+}
+
+function getDate() {
+    var d = new Date(),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
+function getTime() {
+    var d = new Date(),
+        hour = '' + d.getHours(),
+        min = '' + d.getMinutes();
+
+    if (hour.length < 2) hour = '0' + hour;
+    if (min.length < 2) min = '0' + min;
+
+    return [hour, min].join(':');
+}
+
+function updatePrice(){
+    if(currentPage == "register"){
+        if(!$('.manually').checked){
+            var price;
+            switch($('.ticket').val()){
+                case "0":
+                    price = 5;
+                    break;
+                case "1":
+                    price = 10;
+                    break;
+                case "2":
+                    price = 15;
+                    break;
+                case "3":
+                    price = 15;
+                    break;
+                case "4":
+                    price = 20;
+                    break;
+                case "5":
+                    price = 36;
+                    break;
+                case "6":
+                    price = 0;
+                    break;
+            }
+            $('.price').val(price);
+        }
+    }
+}
+
+function updateTickets(){
+    if(currentPage == "register"){
+        var dom;
+        switch($('.color').val()){
+            case "blue":
+                dom = blueTickets;
+                break;
+            case "red":
+                dom = redTickets;
+                break;
+            case "black":
+                dom = blackTickets;
+                break;
+        }
+        $('.ticket').html(dom);
+        updatePrice();
     }
 }
